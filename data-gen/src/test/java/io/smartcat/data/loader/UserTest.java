27e6ef005a7e1934bb5d5a3ca41b3070afc78713
Milan Milosevic <milansm@gmail.com>
2016-12-07 08:40:43 +0100

    Added tests for setting different fields in different builders (#43)

--- data-gen/src/test/java/io/smartcat/data/loader/UserTest.java
+++ data-gen/src/test/java/io/smartcat/data/loader/UserTest.java
@@ -58,47 +58,4 @@ public class UserTest {
 
     }
 
-    // @Test
-    // public void buildRunnerTest() {
-    // RandomBuilder randomUserBuilder = new RandomBuilder(User.class);
-    //
-    // LocalDateTime oldestBirthDate = LocalDateTime.of(1975, 1, 1, 0, 0);
-    // LocalDateTime yougnestBirthDate = LocalDateTime.of(2000, 1, 1, 0, 0);
-    //
-    // randomUserBuilder
-    // .randomFrom("username", "destroyerOfW0rldz", "univerzalBlack", "johnycage", "subzero", "krelac", "pevac")
-    // .randomFrom("firstname", "john", "alice", "bob", "charlie", "david", "elon")
-    // .randomFrom("lastname", "annison", "berkley", "chaplin", "dickinson")
-    // .randomFromRange("numberOfCards", 0L, 14L).randomFromRange("accountBalance", 2.72, 3.14)
-    // .randomSubsetFrom("favoriteMovies", "Predator", "Comandos", "Terminator 2", "Conan", "Red Sonya")
-    //// .randomFromRange("birthDate", oldestBirthDate, yougnestBirthDate)
-    // .toBeBuilt(5);
-    //
-    // RandomBuilder exclusiveUserBuilder = new RandomBuilder(User.class);
-    //
-    // LocalDateTime oldestBirthDateX = LocalDateTime.of(1985, 1, 1, 0, 0);
-    // LocalDateTime yougnestBirthDateX = LocalDateTime.of(1985, 1, 2, 0, 0);
-    //
-    // exclusiveUserBuilder
-    // .exclusiveRandomFrom("username", "destroyerOfW0rldz", "univerzalBlack", "johnycage", "subzero")
-    // .randomFrom("firstname", "john", "alice", "bob", "charlie", "david", "elon")
-    // .randomFrom("lastname", "annison", "berkley", "chaplin", "dickinson")
-    // .randomFromRange("numberOfCards", 0L, 14L).randomFromRange("accountBalance", 2.72, 3.14)
-    // .randomSubsetFrom("favoriteMovies", "Predator", "Comandos", "Terminator 2", "Conan", "Red Sonya")
-    //// .exclusiveRandomFromRange("birthDate", oldestBirthDateX, yougnestBirthDateX)
-    // .toBeBuilt(3);
-    //
-    // BuildRunner runner = new BuildRunner();
-    //
-    // runner.addBuilder(randomUserBuilder);
-    // runner.addBuilder(exclusiveUserBuilder);
-    //
-    // List<User> userList = runner.build();
-    //
-    // userList.stream().forEach(System.out::println);
-    //
-    // Assert.assertEquals(8, userList.size());
-    //
-    // }
-
 }
