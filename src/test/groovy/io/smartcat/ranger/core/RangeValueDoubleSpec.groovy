74c6f1758c01e75d4284869c5ee3464c8269b24c
Vladimir Vajda <vladimir.vajda@smartcat.io>
2017-06-09 09:50:31 +0200

    Removed tests which are testing distribution

--- src/test/groovy/io/smartcat/ranger/core/RangeValueDoubleSpec.groovy
+++ src/test/groovy/io/smartcat/ranger/core/RangeValueDoubleSpec.groovy
@@ -1,15 +1,62 @@
 package io.smartcat.ranger.core
 
-import org.hamcrest.core.Every
+import javax.security.auth.Subject.SecureSet
 
-import io.smartcat.ranger.distribution.UniformDistribution
+import io.smartcat.ranger.distribution.Distribution
 import spock.lang.Specification
-import spock.lang.Unroll
 
-class RangeValueDoubleSpec extends RangeValueSpec {
+class RangeValueDoubleSpec extends Specification {
 
-    @Override
-    def createValue(beginning, end) {
-        new RangeValueDouble(beginning, end)
+    def "cannot create range value when beginning is less than end"() {
+        when:
+        new RangeValueDouble(15.2, 10.01)
+
+        then:
+        thrown(InvalidRangeBoundsException)
+    }
+
+    def "should use distribution's sample method with proper bounds"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextDouble(11.2, 123.3) >> 28.5
+        }
+        def value = new RangeValueDouble(11.2, 123.3, dist)
+
+        when:
+        def result = value.get()
+
+        then:
+        result == 28.5
+    }
+
+    def "calling get multiple times without reset should return same value"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextDouble(11.2, 123.3) >>> 28.5 >> 31.5 >> 18.2 >> 20
+        }
+        def value = new RangeValueDouble(11.2, 123.3, dist)
+        def result = []
+
+        when:
+        10.times { result << value.get() }
+
+        then:
+        result.size() == 10
+        result.every { it == 28.5 }
+    }
+
+    def "multiple reset should not change state"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextDouble(11.2, 123.3) >>> 28.5 >> 31.5 >> 18.2 >> 20
+        }
+        def value = new RangeValueDouble(11.2, 123.3, dist)
+        value.get()
+
+        when:
+        10.times { value.reset() }
+
+        then:
+        value.get() == 31.5 
     }
 }
--- src/test/groovy/io/smartcat/ranger/core/RangeValueLongSpec.groovy
+++ src/test/groovy/io/smartcat/ranger/core/RangeValueLongSpec.groovy
@@ -1,12 +1,61 @@
 package io.smartcat.ranger.core
 
+import io.smartcat.ranger.distribution.Distribution
 import spock.lang.Specification
 import spock.lang.Unroll
 
-class RangeValueLongSpec extends RangeValueSpec {
+class RangeValueLongSpec extends Specification {
 
-    @Override
-    def createValue(beginning, end) {
-        new RangeValueLong(beginning, end)
+    def "cannot create range value when beginning is less than end"() {
+        when:
+        new RangeValueLong(15, 10)
+
+        then:
+        thrown(InvalidRangeBoundsException)
+    }
+
+    def "should use distribution's sample method with proper bounds"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextLong(7, 35) >> 15
+        }
+        def value = new RangeValueLong(7, 35, dist)
+
+        when:
+        def result = value.get()
+
+        then:
+        result == 15
+    }
+
+    def "calling get multiple times without reset should return same value"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextLong(11, 100) >>> 13 >> 31 >> 18 >> 20
+        }
+        def value = new RangeValueLong(11, 100, dist)
+        def result = []
+
+        when:
+        10.times { result << value.get() }
+
+        then:
+        result.size() == 10
+        result.every { it == 13 }
+    }
+
+    def "multiple reset should not change state"() {
+        given:
+        def dist = Mock(Distribution) {
+            nextLong(11, 100) >>> 13 >> 31 >> 18 >> 20
+        }
+        def value = new RangeValueLong(11, 100, dist)
+        value.get()
+
+        when:
+        10.times { value.reset() }
+
+        then:
+        value.get() == 31
     }
 }
